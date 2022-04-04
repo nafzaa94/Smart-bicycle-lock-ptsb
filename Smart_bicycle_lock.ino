a6e0f3bd3312a8197823cfb7b2d9a72c306a7d7d
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2022-04-04 22:03:52 +0800

    Update Smart_bicycle_lock.ino

--- Smart_bicycle_lock.ino
+++ Smart_bicycle_lock.ino
@@ -1,16 +1,22 @@
+#include <ESP32Servo.h> 
+
 #define BLYNK_TEMPLATE_ID           "TMPLj-f8Oiso"
 #define BLYNK_DEVICE_NAME           "Smart bicycle lock"
 #define BLYNK_AUTH_TOKEN            "fLdlufPV0GGYElKpJrH8SDAl5NXmlD05"
 
-
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
 
+Servo myservo;
+
 int vibsensor = 34;
 int resett = 12;
+int signallock = 13;
+int servolock = 27;
 
 int valuevibsensor = 0;
 int valuereset = 0;
+int valuelock = 0;
 int state = 0;
 
 #include <WiFi.h>
@@ -33,6 +39,7 @@ void myTimerEvent()
 {
   valuevibsensor = digitalRead(vibsensor); // 0 1;
   valuereset = digitalRead(resett);
+  valuelock = digitalRead(signallock);
 
   if (valuevibsensor == LOW && state == 0){
     Blynk.logEvent("notification", "ada pencuri");
@@ -42,6 +49,13 @@ void myTimerEvent()
   if (valuereset == HIGH){
     state = 0;
     }
+
+  if (valuelock == HIGH){
+    myservo.write(90);
+    }
+  else{
+    myservo.write(0);
+    }
 }
 
 void setup()
@@ -49,6 +63,15 @@ void setup()
   // Debug console
   Serial.begin(115200);
   pinMode(vibsensor, INPUT);
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);
+  myservo.attach(servolock, 500, 2400);
+
+  myservo.write(0);
 
   Blynk.begin(auth, ssid, pass);
   // You can also specify server:
